a3dcbce59e2a1b607b17b343efac715287da34b9
misha-fedyna <mihajlofedina5@gmail.com>
2024-09-27 15:29:35 +0300

    first master commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab5.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			t = h(2, 3, 4);
+			Assert::AreEqual(t, 0.310345, 0.00001);
+
+		}
+	};
+}
